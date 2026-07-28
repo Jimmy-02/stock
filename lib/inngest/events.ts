e2b736a622e835a6ef9abe8db04abab1a649ece2
Jimmy-02 <tainat1202@gmail.com>
2026-07-28 14:40:15 +0700

    feat: add typed event definitions

---
+++ lib/inngest/events.ts
@@ -0,0 +1,14 @@
+import { eventType, staticSchema } from "inngest";
+
+type UserCreatedPayload = {
+  email: string;
+  fullName: string;
+  country: string;
+  investmentGoals: string;
+  riskTolerance: string;
+  preferredIndustry: string;
+};
+
+export const userCreated = eventType("app/user.created", {
+  schema: staticSchema<UserCreatedPayload>(),
+});
